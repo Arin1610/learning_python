018bd3c04ba49b90e50620ce89cdf33ee39817b1
Arin1610 <100861125+Arin1610@users.noreply.github.com>
2022-04-07 20:52:08 +0530

    Add files via upload

---
+++ Adding a value inside a Tuple.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 27 00:38:12 2022
+
+@author: Arin
+"""
+
+
+
+T1=(1,2,3,4,5,66,7,9,15)
+
+L1=list(T1)
+
+print(T1)
+
+L1.append(100)
+T2=tuple(L1)
+
+print (T2)
