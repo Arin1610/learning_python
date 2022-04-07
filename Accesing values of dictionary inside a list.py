adf314b9db7c9b74d62bee156ee5313dca50f5f7
Arin1610 <100861125+Arin1610@users.noreply.github.com>
2022-04-07 21:03:22 +0530

    Add files via upload

---
+++ Accesing values of dictionary inside a list.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  7 20:51:31 2022
+
+@author: Arin
+"""
+
+d1 = {
+  "Apple": "Red",
+  "Banana": "Yellow",
+  "Pear": "Green",
+  "Grape" : "Purple"
+}
+
+x = d1["Pear"]
+print(x)
